bdcfd0a9eb6b98113b935be777a24e39a11dc9c9
olgapyatunina2024 <olgapyatunina2024@yandex.by>
2024-10-08 18:48:34 +0500

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,6 @@
+def divide(first, second):
+    if second != 0:
+        fun = first/second
+        return fun
+    else:
+        return 'Ошибка'
---
+++ true_math.py
@@ -0,0 +1,8 @@
+from math import inf
+
+def divide(first, second):
+    if second != 0:
+        fun = first/second
+        return fun
+    else:
+        return inf
